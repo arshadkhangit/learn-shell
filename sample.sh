69f22e14db733f4cffad88c3c517eaec89362e03
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-05-25 19:06:04 +0530

    listing down the topics, shell, echo in color

--- sample.sh
+++ sample.sh
@@ -1,4 +1,14 @@
 ls -l
 pwd
 #sudo yum install nginx
+# echo -e "\e[colorm text\e[0m"
+# -e is to enable color
+#\e [colorm to print in particular color
+#\e[0m is to disable color
+#red 31, green 32, yellow 33, blue 34, magneta 35, cyan 36
 echo -e "\e[31m hello world in red color\e[0m"
+echo -e "\e[32m hello world in green color\e[0m"
+echo -e "\e[33m hello world in yellow color\e[0m"
+echo -e "\e[34m hello world in blue color\e[0m"
+echo -e "\e[35m hello world in redmagneta color\e[0m"
+echo -e "\e[36m hello world in cyan color\e[0m"
