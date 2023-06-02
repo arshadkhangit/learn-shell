dfa7a8318adba315345a4e048050ffd820f4a5bc
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-06-02 10:47:58 +0530

    loops

--- loops.sh
+++ loops.sh
@@ -1,4 +1,12 @@
-for a in 'seq 10'
-do
+a=10
+while [ $a -gt 0 ]; do
+  echo $a
+  a=$(($a-1))
+  sleep 1
+done
+
+for component in catalogue user cart frontend shipping payment ; do
+  echo creating component - $component
+  sleep 1
 done
 
