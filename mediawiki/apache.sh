0f70c856be794b24c888c9ca58fef127473344e0
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-06-03 11:20:37 +0530

    changing file name

--- mediawiki/apache.sh
+++ mediawiki/apache.sh
@@ -1,7 +1,7 @@
 cd /tmp
 url=https://releases.wikimedia.org/mediawiki/1.39/mediawiki-1.39.3.tar.gz
 curl -O $url
+folder=$(echo $url | awk -F / '{print $6}' | sed 's/.tar.gz//')
 yum install httpd -y
 cd /var/www/html
 tar -xf /tmp/mediawiki-1.39.3.tar.gz
-echo $url | awk -F / '{print $6}' | sed 's/.tar.gz//'
